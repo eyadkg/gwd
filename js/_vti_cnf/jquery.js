fcff695e6f0b52e9442009a5410c4bd4e343d4c4
EKGWD <30369275+EKGWD@users.noreply.github.com>
2017-10-13 12:27:38 +0300

    a
    echo "# gwd" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git remote add origin https://github.com/eyadkg/gwd.git
    git push -u origin master

---
+++ js/_vti_cnf/jquery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-LQA9Q2H\\eyado
+vti_modifiedby:SR|DESKTOP-LQA9Q2H\\eyado
+vti_timelastmodified:TR|16 Jun 2017 02:47:13 -0000
+vti_timecreated:TR|15 Jun 2017 16:30:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|main.html
+vti_cacheddtm:TX|16 Jun 2017 02:47:13 -0000
+vti_filesize:IR|268039
+vti_nexttolasttimemodified:TR|15 Jun 2017 16:30:32 -0000
